e808a29ba42ee436f20e7f561b3d46c8ae683a7d
qiqetes <qiqete@gmail.com>
2019-05-01 01:11:54 +0200

    Added new way to draw paths

--- SvgCreator/Draw.cs
+++ SvgCreator/Draw.cs
@@ -21,12 +21,30 @@ namespace SvgCreator
         }
         public static string DrawLine(Point p1, Point p2, double stroke)
         {
-            return @"<path id = ""linefrom"" d = ""m " + p1.X + "," + p1.Y + " " + (p2.X - p1.X) + "," + (p2.Y - p1.Y) + @""" style=""fill: none; stroke:#000000;stroke-width:"+stroke.ToString()+@"px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"" />";
+            return @"<path id = ""linefrom"" d = ""m " + p1.X + "," + p1.Y + " " + (p2.X - p1.X) + "," + (p2.Y - p1.Y) + @""" style=""fill: none; stroke:#000000;stroke-width:"+stroke+@"px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"" />";
         }
 
         public static string DrawLine(Point p1, Point p2, double stroke, string strokeColor)
         {
             return @"<path id = ""linefrom"" d = ""m " + p1.X + "," + p1.Y + " " + (p2.X - p1.X) + "," + (p2.Y - p1.Y) + @""" style=""fill: none; stroke:"+ strokeColor + @";stroke-width:" + stroke.ToString() + @"px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"" />";
         }
+
+        public static string StraightPath(Point[] points) {
+            String path = @"<path id = ""linefrom"" style=""fill:; stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"" d=""";
+            path += "m " + points[0].Y + "," + points[0].X + " ";
+            for (int i = 1; i < points.Length; i++) {
+                path += (points[i].Y - points[i-1].Y) + "," + (points[i].X - points[i-1].X) + " ";
+            }
+            return path + @"z"" />";
+        }
+        public static string StraightPath(Point[] points, int stroke, string strokeColor) {
+            String path = @"<path id = ""linefrom"" style=""fill: red; stroke:" + strokeColor +@";stroke-width:"+ stroke +@"px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"" d=""";
+            path += "m " + points[0].Y + "," + points[0].X + "l ";
+            for (int i = 1; i < points.Length; i++) {
+                path += (points[i].Y - points[i - 1].Y) + "," + (points[i].X - points[i - 1].X) + " ";
+            }
+            return path + @""" />";
+        }
+
     }
 }
--- SvgCreator/Program.cs
+++ SvgCreator/Program.cs
@@ -8,19 +8,20 @@ namespace SvgCreator
     {
         static void Main(string[] args)
         {
-            string path = "D:\\Qiqete\\Documents\\Dev\\csharp\\SvgCreator\\test.svg";
+            string path = @"E:\Users\Qiqete\Documents\Dev\Procedural-Svg-Drawings\test.svg";
             int width = 1000;
             int height = 1000;
 
             // Generate random points:
             Point[] points = new Point[100];
             Point[] pointsR = new Point[100];
-            for (int i = 0; i < points.Length; i++)
-            {
-                //points[i] = new Point();
+            for (int i = 0; i < points.Length; i++) {
+                //Random r = new Random();
+                //int randomRadius = r.Next(100);
+                points[i] = new Point();
                 points[i] = new Point(
-                    (int)(500 + (250 * Math.Cos(2 * Math.PI / points.Length * i))),
-                    (int)(500 + (250 * Math.Sin(2 * Math.PI / points.Length * i))));
+                    (int)(500 + ((100 + 150) * Math.Cos(2 * Math.PI / points.Length * i))),
+                    (int)(500 + ((100 + 150) * Math.Sin(2 * Math.PI / points.Length * i))));
             }
             // Generate random points on a circular matrix
             for (int i = 0; i < pointsR.Length; i++)
@@ -28,8 +29,8 @@ namespace SvgCreator
                 Random r = new Random();
                 int randomRadius = r.Next(200) + 150;
                 pointsR[i] = new Point(
-                    (int)(500 + (randomRadius * Math.Cos(2 * Math.PI / points.Length * i + 2 * Math.PI / points.Length / 2))),
-                    (int)(500 + (randomRadius * Math.Sin(2 * Math.PI / points.Length * i + 2 * Math.PI / points.Length / 2))));
+                    (int)(500 + (randomRadius * Math.Cos(2 * Math.PI / pointsR.Length * i + 2 * Math.PI / pointsR.Length / 2))),
+                    (int)(500 + (randomRadius * Math.Sin(2 * Math.PI / pointsR.Length * i + 2 * Math.PI / pointsR.Length / 2))));
             }
 
 
@@ -40,23 +41,10 @@ namespace SvgCreator
             using (StreamWriter writer = new StreamWriter(path, true))
             {
                 writer.Write(Utils.startOfSvg);
-                // Todo: Write here every path
-                for (int i = 0; i < points.Length; i++)
-                {
-                    
-                    //writer.WriteLine(Draw.DrawCircle(points[i]));
-                    if(i != points.Length - 1)
-                    {
-                        //Random r = new Random();
-                        writer.WriteLine(Draw.DrawLine(points[i], pointsR[i], 1.5));
-                        writer.WriteLine(Draw.DrawLine(pointsR[i], points[i+1], 1.5));
-                    }
-                    else
-                    { 
-                        writer.WriteLine(Draw.DrawLine(points[i], pointsR[i], 1.5));
-                        writer.WriteLine(Draw.DrawLine(pointsR[i], points[0], 1.5));
-                    }
-                }
+
+                writer.Write(Draw.StraightPath(points));
+                writer.Write(Draw.StraightPath(pointsR, 1, "red"));
+
                 writer.Write(Utils.endOfSvg);
             }
         }
